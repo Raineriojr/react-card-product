f6bbba345efc36926f23128c89f9ec0e945f38cb
Raineriojr <raijunior20@gmail.com>
2023-01-12 12:12:12 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+// **Assets
+import Sofa from './assets/sofa.jpg';
+import GifSofa from './assets/gifsofa.gif';
+import Rotate from './assets/icons/rotate.png';
+import Close from './assets/icons/close.png'
+
 function App() {
+  const [isActiveRotation, setActiveRotation] = React.useState<boolean>(false);
+
+  const toggleRotationState = () => setActiveRotation(!isActiveRotation);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='card'>
+        <div>
+          <img
+            id='rotateIcon'
+            src={isActiveRotation ? Close : Rotate}
+            onClick={toggleRotationState}
+          />
+          {isActiveRotation
+            ?
+            <img id='gifSofa' src={GifSofa} />
+            :
+            <img id='sofa' src={Sofa} />
+          }
+        </div>
+
+        <div className='descriptionContainer'>
+          <p id='code'>CÓDIGO: 42404</p>
+          <h1>Sofá Margot II - Rosé</h1>
+          <p id='value'>R$ 4.000</p>
+
+          <button>
+            <p>ADICIONAR À CESTA</p>
+          </button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
